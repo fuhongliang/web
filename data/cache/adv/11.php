baf96906dfd6294f6c937425e41d9ab21b53500f
AdrianLi621 <1772702063@qq.com>
2019-04-02 17:15:48 +0800

    v

--- data/cache/adv/11.php
+++ data/cache/adv/11.php
@@ -18,7 +18,7 @@
       'adv_id' => '935',
       'ap_id' => '11',
       'adv_title' => '首页物流自提banner',
-      'adv_content' => 'a:2:{s:7:"adv_pic";s:21:"04876370813599010.gif";s:11:"adv_pic_url";s:41:"test.heirui.cn/delivery/index.php?act=login";}',
+      'adv_content' => 'a:2:{s:7:"adv_pic";s:21:"04876370813599010.gif";s:11:"adv_pic_url";s:41:"test.haoid.cn/delivery/index.php?act=login";}',
       'adv_start_date' => '1434211200',
       'adv_end_date' => '1749312000',
       'slide_sort' => '0',
