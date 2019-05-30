5373df5a3eafa21af90121db276d7d2b87ef095d
“ame <1772702063@qq.com>
2019-05-30 10:09:27 +0800

    v

--- admin/templates/default/layout/blank_layout.php
+++ admin/templates/default/layout/blank_layout.php
@@ -1,13 +1,15 @@
-<?php defined('InShopNC') or exit('Access Invalid!');?>
+<?php defined('InShopNC') or exit('Access Invalid!'); ?>
 <!doctype html>
-<html><head>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET;?>">
-<title><?php echo $output['html_title'];?></title>
-<script type="text/javascript" src="<?php echo RESOURCE_SITE_URL;?>/js/jquery.js"></script>
+<html>
+<head>
+    <meta name=referrer content=no-referrer>
+    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>">
+    <title><?php echo $output['html_title']; ?></title>
+    <script type="text/javascript" src="<?php echo RESOURCE_SITE_URL; ?>/js/jquery.js"></script>
 </head>
 <body>
-<?php 
-	require_once($tpl_file);
+<?php
+require_once($tpl_file);
 ?>
 </body>
 </html>
