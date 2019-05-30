1ca828e610b7a246f5bd325f12a26ea925400193
AdrianLi621 <1772702063@qq.com>
2019-05-30 10:25:28 +0800

    v

--- data/cache/adv/1047.php
+++ data/cache/adv/1047.php
@@ -1,16 +1,4 @@
 <?php defined('InShopNC') or exit('Access Invalid!'); return array (
-  'ap_id' => '1047',
-  'ap_name' => '首页中上部banner',
-  'ap_intro' => '好商城V4',
-  'ap_class' => '0',
-  'ap_display' => '2',
-  'is_use' => '1',
-  'ap_width' => '1200',
-  'ap_height' => '90',
-  'ap_price' => '100',
-  'adv_num' => '1',
-  'click_num' => '0',
-  'default_content' => '04891676111561966.png',
   'adv_list' => 
   array (
     0 => 
--- data/cache/adv/9.php
+++ data/cache/adv/9.php
@@ -1,16 +1,4 @@
 <?php defined('InShopNC') or exit('Access Invalid!'); return array (
-  'ap_id' => '9',
-  'ap_name' => '首页底部通栏图片广告',
-  'ap_intro' => '位于首页底部的通栏型图片广告',
-  'ap_class' => '0',
-  'ap_display' => '2',
-  'is_use' => '1',
-  'ap_width' => '1200',
-  'ap_height' => '90',
-  'ap_price' => '100',
-  'adv_num' => '1',
-  'click_num' => '0',
-  'default_content' => '04418235791378401.jpg',
   'adv_list' => 
   array (
     0 => 
